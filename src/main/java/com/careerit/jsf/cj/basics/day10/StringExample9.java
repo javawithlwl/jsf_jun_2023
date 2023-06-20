3f2032b6eda6c27d81c38352f897205a44199b02
javawithlwl <javawithlwl@gmail.com>
2023-06-20 07:16:45 +0530

    J1001-fixed sort issue in Anagram method

--- src/main/java/com/careerit/jsf/cj/basics/day10/StringExample9.java
+++ src/main/java/com/careerit/jsf/cj/basics/day10/StringExample9.java
@@ -19,6 +19,8 @@ public class StringExample9 {
     }
     char[] arr1 = s1.toCharArray();
     char[] arr2 = s2.toCharArray();
+    Arrays.sort(arr1);
+    Arrays.sort(arr2);
     return Arrays.equals(arr1, arr2);
   }
 }
